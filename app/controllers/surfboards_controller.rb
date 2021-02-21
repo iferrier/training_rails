28ff49a2d7f7720555c4a9e9aaaeea05041495c2
Isabelle Ferrier <ferrierisabelle@gmail.com>
2021-02-21 20:02:06 +0000

    Add home page

--- app/controllers/surfboards_controller.rb
+++ app/controllers/surfboards_controller.rb
@@ -2,4 +2,23 @@ class SurfboardsController < ApplicationController
   def index
     @surfboards = Surfboard.all
   end
+
+  def new
+    @surfboard = Surfboard.new
+  end
+
+  def create
+    @surfboard = Surfboard.new(surfboard_params)
+    if @surfboard.save
+      redirect_to surfboards_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def surfboard_params
+    params.require(:surfboard).permit(:name, :price)
+  end
 end
